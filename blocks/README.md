60f006564cf404a2b567aa4c0028376aea2f9113
ViktorVP <79720052+ViktorVP@users.noreply.github.com>
2021-05-22 17:44:04 +0300

    mesto

---
+++ blocks/README.md
@@ -0,0 +1,13 @@
+# Project "Mesto"
+### it is the fifth project of Yandex Practicum. <br/>
+It is my first project designed with Java Script. I realised forms validation. <br/>
+It is look like old version of Instagram). <br/>
+You can use this page in current version of FireFox, Google Chrome, Yandex Browser. <br/>
+#### Technologies:
+* HTML 
+* CSS (flexbox, grid, BEM) <br/>
+* JavaScript.
+#### Link: 
+https://viktorvp.github.io/mesto/
+
+
--- blocks/scripts/index.js
+++ blocks/scripts/index.js
@@ -1,12 +1,12 @@
-const openPopupButton = document.querySelector('.profile__editbutton'); //кнопка открытия попапа редактирования профиля
-const popupEdit = document.querySelector('.popup_type_edit');// попап редактирования профиля
-const closePopupButton = document.querySelector('.popup__close_type_edit');//кнопка закрытия  попапа редактирования профиля
+const openEditPopupButton = document.querySelector('.profile__editbutton'); //кнопка открытия попапа редактирования профиля
+const popupEdit = document.querySelector('.popup_type_edit');//попап редактирования профиля
+const closeEditPopupButton = document.querySelector('.popup__close_type_edit');//кнопка закрытия  попапа редактирования профиля
 const userName = document.querySelector('.popup__input_type_profilename');//поле ввода имения пользователя
 const userPosition = document.querySelector('.popup__input_type_profiledescription')//поля ввода описания профиля
 const popupButton = document.querySelector('.popup__button_type_edit');//кнопка сохранить профиль пользователя
 const profileName = document.querySelector('.profile__name');//текст имени пользователя в html
 const profilePosition = document.querySelector('.profile__position');//текст описания профиля в html
-const popupForm = document.querySelector('.popup__form_type_edit');//форма попапв редактирования профиля
+const popupEditForm = document.querySelector('.popup__form_type_edit');//форма попапв редактирования профиля
 const openAddCardButton = document.querySelector('.profile__addbutton');//кнопка добавления карточки
 const popupAddCard = document.querySelector('.popup_type_addcard');//попап добавления карточки
 const popupAddCardForm = document.querySelector('.popup__form_type_addcard')//форма попапа добавления карточки
@@ -19,8 +19,17 @@ const elementLike = document.querySelector('.element__like');//кнопка "л�
 const closeBigPopupButton = document.querySelector('.popup__close_type_picturezoom')//кнопка закрытия попапа увеличенной картинки
 const popupPictureZoom = document.querySelector('.popup__picture');//увеличенная картинка
 const popupPictureSign = document.querySelector('.popup__picturesign');//подпись под увеличенной картинкой
-const popupBig = document.querySelector('.popup_type_zoom');
-const popups = document.querySelectorAll('.popup')
+const сardTemplate = document.querySelector('#card-template').content;//Template карточки
+const popupBig = document.querySelector('.popup_type_zoom');//попап увеличенной картинки
+const popups = document.querySelectorAll('.popup')//псевдомассив попапов
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+  }
 //массив карточек
 const initialCards = [
   {
@@ -49,20 +58,12 @@ const initialCards = [
   }
 ];
 
+enableValidation(config);
 
 //функция открытия
 function open(popup) {
   popup.classList.add('popup_is-opened');
-  document.addEventListener('keydown', closeEscape);;
-  closeError()
-  enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__input-error_active'
-  });
+  document.addEventListener('keydown', closeEscape)
 }
 
 //функция закрытия
@@ -72,7 +73,7 @@ function close(popup) {
 };
 
 // закрытие по клику на оверлэй
-popups.forEach(popupElement => popupElement.addEventListener('click', function (evt) {
+popups.forEach(popupElement => popupElement.addEventListener('mousedown', function (evt) {
   if (evt.target === evt.currentTarget) {
     close(popupElement);
   }
@@ -86,25 +87,9 @@ function closeEscape(evt) {
   };
 };
 
-function closeError() {
-  const formList = document.querySelectorAll('.popup');
-  console.log('formList', formList)
-  formList.forEach(function () {
-    const inputList = document.querySelectorAll('.popup__input');
-    inputList.forEach(function (inputElement) {
-      console.log('inputList', inputList)
-      inputElement.classList.remove('popup__input_type_error');
-      inputElement.value = '';
-    });
-    const spanText = document.querySelectorAll('.popup__input-error');
-    spanText.forEach(function (item) {
-      item.textContent = '';
-    })
-  })
-};
-
 //вызов функции открытия попапа редактирования профиля
-openPopupButton.addEventListener('click', function () {
+openEditPopupButton.addEventListener('click', function () {
+  cleanInput(popupEdit, config);
   open(popupEdit);
   userName.value = profileName.textContent;
   userPosition.value = profilePosition.textContent;
@@ -112,12 +97,13 @@ openPopupButton.addEventListener('click', function () {
 
 //вызов функции открытия попапа добавления карточки
 openAddCardButton.addEventListener('click', function () {
+  cleanInput(popupAddCard, config);
   open(popupAddCard);
 });
 
 //вызов функции закрытия попапа редактирования профиля
-closePopupButton.addEventListener('click', function () {
-  close(popupEdit)
+closeEditPopupButton.addEventListener('click', function () {
+  close(popupEdit);
 });
 
 //вызов функции закрытия попапа добавления карточки
@@ -133,7 +119,7 @@ closeBigPopupButton.addEventListener('click', function () {
 
 
 //функция отправки формы редактирования профиля
-function formSubmitHandler(evt) {
+function submitEditFormHandler(evt) {
   evt.preventDefault();
   profileName.textContent = userName.value;
   profilePosition.textContent = userPosition.value;
@@ -141,7 +127,7 @@ function formSubmitHandler(evt) {
 };
 
 //вызов функции отправки формы редактирования профиля
-popupForm.addEventListener('submit', formSubmitHandler);
+popupEditForm.addEventListener('submit', submitEditFormHandler);
 
 //функция показа увеличенной картинки
 function openBigImage(name, link) {
@@ -151,24 +137,25 @@ function openBigImage(name, link) {
   open(popupBig);
 };
 
+//удаление
+function removeCard(e) {
+  e.target.closest('.element').remove();
+}
+
 //функция создания карточки
 function createCard(name, link) {
-  const сardTemplate = document.querySelector('#card-template').content;
   const сardElement = сardTemplate.querySelector('.element').cloneNode(true);
-  сardElement.querySelector('.element__text').textContent = name;
-  сardElement.querySelector('.element__image').src = link;
-  сardElement.querySelector('.element__image').alt = 'Ваше изображение';
-  //увеличение
   const cardImage = сardElement.querySelector('.element__image');
+  сardElement.querySelector('.element__text').textContent = name;
+  cardImage.src = link;
+  cardImage.alt = 'Ваше изображение';
+  //увеличение
   cardImage.addEventListener('click', function () { openBigImage(name, link) });
   //лайк
   сardElement.querySelector('.element__like').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__like_active')
   });
-  //удаление
-  function removeCard(e) {
-    e.target.closest('.element').remove();
-  }
+  
   сardElement.querySelector('.element__delete').addEventListener('click', removeCard);
   return сardElement;
 };
